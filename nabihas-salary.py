382a5c9496de5320f3d647c76b49daad7e8ffa11
Ahmad shamsedine <ahmadshamsedine@gmail.com>
2025-02-19 13:19:22 +0200

    Prompts the user to enter the amounts for savings, rent, and electricity, and calculates the total amount.

---
+++ nabihas-salary.py
@@ -0,0 +1,7 @@
+print("Hello Nabiha welcome to your monthly report ")
+
+def get_amounts(salary):
+    """
+    Prompts the user to enter the amounts for savings, rent, and electricity, and calculates the total amount.
+    """
+    print("Nabiha, please enter the following amounts: ")
